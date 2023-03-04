20b0d80b5536e0d58a3bdecc593a635bab857b7f
ThanhOwO <91793770+ThanhOwO@users.noreply.github.com>
2023-03-04 16:34:24 +0700

    Game loop

--- app/src/main/java/com/example/a2dshootergame/MainActivity.java
+++ app/src/main/java/com/example/a2dshootergame/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.a2dshootergame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
+/**
+ * MainActivity is the entry point to application.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //set window fullscreen (hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        //set content view to game to render object to the screen
+        setContentView(new Game(this));
     }
 }
